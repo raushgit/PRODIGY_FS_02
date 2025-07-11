6992b8e72595a94e51bcdb4a928e73e811eee9c8
raushgit <raushforlife@gmail.com>
2025-07-11 14:45:15 +0530

    Changes made

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,3 +1,7 @@
+// File: frontend/src/App.test.js
+// --- a/file:///e%3A/Prgrams/Prodigy%20Internship/PRODIGY_FS_O2/frontend/src/App.test.js
+// +++ b/file:///e%3A/Prgrams/Prodigy%20Internship/PRODIGY_FS_O2/frontend/src/App.test.js
+// @@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
