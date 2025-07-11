f6f82ebb84d778bdf618860fc5e3d18c1c596418
raushgit <raushforlife@gmail.com>
2025-07-11 14:33:40 +0530

    Utils changes

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,5 +1,6 @@
 import { toast } from 'react-toastify';
-
+//This is the utilit file for the frontend application
+//Everything that is common to the frontend application should be placed here
 export const notify = (message, type) => {
     toast[type](message);
 }
