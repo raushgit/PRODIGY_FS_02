5072998b26337ca7bd528dc8e8fe46aee7ccbda3
raushgit <raushforlife@gmail.com>
2025-07-11 14:51:07 +0530

    Changes made

--- backend/Routes/EmployeeRoutes.js
+++ backend/Routes/EmployeeRoutes.js
@@ -6,8 +6,13 @@ const { createEmployee,
 } = require('../Controllers/EmployeeController');
 const { cloudinaryFileUploader } = require('../Middlewares/FileUplaoder');
 
+// File: backend/Routes/EmployeeRoutes.js
+// This file defines the routes for employee management
 const router = require('express').Router();
 
+
+// Define the routes for employee management
+// These routes handle the creation, retrieval, updating, and deletion of employee records
 router.get('/', getAllEmployees)
 router.get('/:id', getEmployeeById)
 router.delete('/:id', deleteEmployeeById)
