df9cb9ea4c042d83d40b581ad3f35d784796a69b
raushgit <raushforlife@gmail.com>
2025-07-11 14:46:12 +0530

    Changes made

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,3 +1,10 @@
+// This is the reportWebVitals file for the frontend application
+// It is used to measure the performance of the application
+// It exports a function that takes a callback function as an argument
+// The callback function is called with the performance metrics of the application
+// The metrics are collected using the web-vitals library
+// The web-vitals library is imported dynamically to avoid bundling it with the main application code
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
